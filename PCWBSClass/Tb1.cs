0b06f49db25609ae1ab4d9d4e54218c4ee535dea
kkb-hub <hiroki5641g@gmail.com>
2023-01-16 22:37:22 +0900

    Stored Procedure対応DBを利用

---
+++ PCWBSClass/Tb1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entityclass
+{
+    public class Tb1 : BaseClass
+    {
+
+        public long? FKTb2 { get; set; }
+        public string? b { get; set; }
+        public string? c { get; set; }
+        public string? d { get; set; }
+        public string? e { get; set; }
+
+
+    }
+}
--- RazorPJ/Controllers/PCWBSDataController.cs
+++ RazorPJ/Controllers/PCWBSDataController.cs
@@ -7,20 +7,20 @@ using Razor.Models;
 namespace Razor.Controllers
 {
 
-    public class PCWBSwithBaseController : ApiBaseController<PCWBSDbContext, EntytyA>
+    public class PCWBSwithBaseController : ApiBaseController<PCWBSDbContext, Tb1>
     {
         public PCWBSwithBaseController(PCWBSDbContext context) : base(context)
         {
         }
-        public DbSet<EntytyA> EntityA { get; set; }
-        public DbSet<EntytyA> EntityB { get; set; }
+        public DbSet<Tb1> Tb1 { get; set; }
+        public DbSet<Tb1> Tb2 { get; set; }
     }
 
-    public class EntityBwithBaseController : ApiBaseController<PCWBSDbContext, EntityB>
+    public class EntityBwithBaseController : ApiBaseController<PCWBSDbContext, Tb2>
     {
         public EntityBwithBaseController(PCWBSDbContext context) : base(context)
         {
         }
-        public DbSet<EntytyA> EntityB { get; set; }
+        public DbSet<Tb2> Tb2 { get; set; }
     }
 }
